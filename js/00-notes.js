0b4297426da77d175b8f7c259c976cccc6617c83
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-09 13:54:17 +0200

    js-intro

---
+++ js/00-notes.js
@@ -0,0 +1,7 @@
+
+// kintamieji - jo atmintyje yra laikoma informacija
+// salygos sakiniai - issakoti logika pagal ka daryti kai daugiau/maziau, tiesa/melas
+// ciklai - kai turime sarasa su informacija kuri yra vienodos strukturos
+// funkcijos - aprasome logika kuria norime perpanaudoti N kartu, tik su galimai skirtingais parametrais
+
+// event`ai - (events listeners) - skrta stebeti ivykius (click, sroll, keypress ir pan) i kuriuos reikia sureguoti iskvieciant norima finkcija
---
+++ js/01-variables.js
@@ -0,0 +1,64 @@
+const a = 'Labas😜'
+console.log(a);
+console.log("Kai diena");
+console.log('Graži Lietuva');
+console.log(123579);
+console.log('Mano', 'Vardas', 'yra');
+console.log('Mano Vardas yra');
+
+console.log('--------------------');
+
+/* Kintamuju INICIAVIMO BUDAI:
+    const - kintamasis, kurio reiksmes negalime is naujo priskirti*
+    let - kintamasis, kurio reiksmes gali buti priskirta kiek norite kartu (is naujo)
+    var - esant galimybe ⚠⚠⚠⚠⚠NENAUDOTI⚠⚠⚠⚠⚠!!!!   
+
+    * - yra papildomu salugu su kompleksiniais kintamaisiais
+
+*/
+
+let vardas = 'Vakaris';
+console.log(vardas);
+
+const pasisveikinimas = 'Labas dienas - Lietuva 🟨🟩🟥';
+console.log(pasisveikinimas);
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 77;
+console.log();
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+console.log(suma);
+
+console.log(suma + pirmasSkaicius + antrasSkaicius);
+console.log(suma, '+', pirmasSkaicius, '+', antrasSkaicius,);
+
+const mathText = pirmasSkaicius + ' + ' + antrasSkaicius + ' =' + suma;
+console.log(mathText);
+
+console.log('---------------------');
+
+const petrasName = 'Petras';
+const age  ='99';
+
+// Petras age is 99.
+
+const petrasAge = petrasName + ' age is ' + age + '.';
+console.log(petrasAge);
+
+
+console.log('---------------------');
+
+// sarasa (array)
+const pazymiai = [5, 5, 8, 9, 10, 6];
+console.log(pazymiai);
+
+const abc = ["a", "l", "p", "f", 'd', 'e',];
+console.log(abc);
+
+const pirmasPazymys = pazymiai [0];
+console.log(pirmasPazymys);
+
+console.log(pazymiai);
