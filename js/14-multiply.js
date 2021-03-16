3a33b42e0c70b2ca50c2a3cfaeb039fde50ea946
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-16 19:55:37 +0200

    Multiply

--- js/14-multiply.js
+++ js/14-multiply.js
@@ -1,9 +1,9 @@
 function daugyba (a, b) {
-    const rez = a * c;
+    const rez = a * b;
     return rez;
 }
 
 
 console.log( daugyba(4, 0));
 console.log( daugyba(4, 5));
-console.log( daugyba(4, 5));
+console.log( daugyba(7, 5));
