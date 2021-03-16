972aee816fa14460609d2d369bfc8ffdbe6a3d30
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-16 19:16:38 +0200

    functions

---
+++ js/12-marks.js
@@ -0,0 +1,35 @@
+function pazymiuVidurkis(vardas, pazymiai) {
+    let suma = 0;
+    
+    for (let i = 0; i < pazymiai.length; i++ ) {
+        suma += pazymiai[i];
+    }
+
+    const average = suma / pazymiai.length;
+
+    console.log(`${vardas}: pazymiu vidrkis yra ${average}.`);
+}
+
+// pazymiuVidurkis('Petriukas', [10, 2, 8, 4]);
+// pazymiuVidurkis('Maryte', [10, 8, 6, 8]);
+// pazymiuVidurkis('Alfonsas', [10, 8, 10, 8]);
+
+console.log('----------------');
+
+const mokykla = [
+    ['Petriukas', [10, 2, 8, 4]],
+    ['Maryte', [10, 8, 6, 8]],
+    ['Alfonsas', [10, 8, 10, 8]],
+];
+// console.log(mokykla);
+
+for (let i = 0; i < mokykla.length; i++) {
+    console.log('------------');
+    const mokinys = mokykla[i];
+    const name = mokinys[0];
+    const marks = mokinys[1];
+    
+    pazymiuVidurkis(name, marks);
+    pazymiuVidurkis(mokykla[i][0], mokykla[i][1])
+    
+}
