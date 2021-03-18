303d8967c7e67fedc30790ebc2415e18edb162e3
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-18 15:16:55 +0200

    opposite number

--- codewars/opposite number.js
+++ codewars/opposite number.js
@@ -3,9 +3,11 @@ https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
 */
 
 function opposite(number) {
-        return opposite; 
+      const opp = number * -1;
+      return (opp);
+     
   }
-  console.log(opposite(1))
-  console.log(opposite(-1), -1,)
-  console.log(opposite(0), -1,)
-  console.log(opposite(3.5), -3,5,)
+  console.log(opposite(7))
+  console.log(opposite(1), -1,)
+  console.log(opposite(-0), -1,)
+  console.log(opposite(-5.5), -3,5,)
