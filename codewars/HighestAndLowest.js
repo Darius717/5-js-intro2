1e12794f12472606cfb16956fb59c49ca08b5fa6
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-18 17:00:49 +0200

    Highest and lowest

---
+++ codewars/HighestAndLowest.js
@@ -0,0 +1,10 @@
+function highAndLow(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        numbers = numbers.split(' ');
+        return numbers;
+    }
+}
+    
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
+
+//Kol kas dar nea :)
