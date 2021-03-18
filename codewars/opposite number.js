75a7da2aed93e9dc6fd02c2699a32e0cd12ba02d
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-18 11:36:48 +0200

    digits count

---
+++ codewars/opposite number.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+*/
+
+function opposite(number) {
+        return opposite; 
+  }
+  console.log(opposite(1))
+  console.log(opposite(-1), -1,)
+  console.log(opposite(0), -1,)
+  console.log(opposite(3.5), -3,5,)
---
+++ js/15-digits.js
@@ -0,0 +1,59 @@
+function digits(skaicius) {
+    //input validation
+if (typeof skaicius !== 'number') {
+    return 'ERROR: Turi buti skaiciaus tipo';
+}
+
+if ('' + skaicius === 'Nan') {
+    return 'ERROR: NaN nera narmalus skaicius'
+}
+
+if (isNaN(skaicius)) {
+
+}
+
+if (!isFinite(skaicius)) {
+    return `ERROR: ${skaicius} nera normalus skaicius`
+}
+
+//logic
+const absoliutas = Math.abs(skaicius);
+const tekstinisSkaicius = '' +  absoliutas;
+let skaitmenuKiekis = tekstinisSkaicius.length;
+
+//jei skaicius turi desimtaine daly tai sumazine 1 vienetu
+
+if ( skaicius % 1 !==0){
+    skaitmenuKiekis--;
+}
+
+//jei skaicius yra neigiamas, tai sumaziname 
+
+//return result
+
+return skaitmenuKiekis;
+
+}
+
+console.log(digits( true ) );
+console.log(digits( 'asd' ));
+console.log(digits( NaN ));
+console.log(digits( Infinity ));
+console.log(digits( -Infinity ));
+console.log(digits( ));
+console.log(digits( undefined ));
+console.log(digits( null ));
+console.log(digits( [] ));
+console.log(digits( '' ));
+console.log(digits(digits));
+console.log(digits( [4624] ));
+console.log(digits( 5 ), 1);
+console.log(digits( 781 ), 3);
+console.log(digits( 37060123456 ), 11);
+console.log(digits( 3.1456 ), 3);
+console.log(digits( -3.1456 ), 3);
+console.log(digits( -456 ), 3);
+console.log(digits( +3.1456 ), 3);
+
+console.log('Koks sprendimas norint rasti sandartiens saiciaus israiskas turimu skaitmenu kiekis');
+console.log(digits( 10000000000000000000000000000000000000000 ), 21);
