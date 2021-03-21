45f36140f8d2dac533a748bc95b053cd38d35aab
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-21 18:32:11 +0200

    digits and codewars

--- codewars/StringtToAnArray.js
+++ codewars/StringtToAnArray.js
@@ -9,3 +9,6 @@ return (arr);
 
 console.log(stringToArray("Robin Singh"), ["Robin", "Singh"]);
 console.log(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"])
+
+
+//return string.split(' ');
--- js/15-digits.js
+++ js/15-digits.js
@@ -8,27 +8,25 @@ if ('' + skaicius === 'Nan') {
     return 'ERROR: NaN nera narmalus skaicius'
 }
 
-if (isNaN(skaicius)) {
-
-}
 
 if (!isFinite(skaicius)) {
     return `ERROR: ${skaicius} nera normalus skaicius`
 }
 
 //logic
-const absoliutas = Math.abs(skaicius);
-const tekstinisSkaicius = '' +  absoliutas;
+const tekstinisSkaicius = '' +  skaicius;
 let skaitmenuKiekis = tekstinisSkaicius.length;
 
-//jei skaicius turi desimtaine daly tai sumazine 1 vienetu
+//jei skaicius turi desimtaine dali, tai sumaziname 1 vienetu
 
 if ( skaicius % 1 !==0){
     skaitmenuKiekis--;
 }
 
 //jei skaicius yra neigiamas, tai sumaziname 
-
+if (skaicius < 0) {
+    skaitmenuKiekis--;
+}
 //return result
 
 return skaitmenuKiekis;
@@ -44,16 +42,19 @@ console.log(digits( ));
 console.log(digits( undefined ));
 console.log(digits( null ));
 console.log(digits( [] ));
+console.log(digits( [4624] ));
 console.log(digits( '' ));
 console.log(digits(digits));
 console.log(digits( [4624] ));
 console.log(digits( 5 ), 1);
 console.log(digits( 781 ), 3);
 console.log(digits( 37060123456 ), 11);
-console.log(digits( 3.1456 ), 3);
-console.log(digits( -3.1456 ), 3);
+console.log(digits( 3.1456 ), 5);
+console.log(digits( -3.1456 ), 5);
 console.log(digits( -456 ), 3);
-console.log(digits( +3.1456 ), 3);
+console.log(digits( +3.1456 ), 5);
 
 console.log('Koks sprendimas norint rasti sandartiens saiciaus israiskas turimu skaitmenu kiekis');
-console.log(digits( 10000000000000000000000000000000000000000 ), 21);
+console.log(digits( 10000000000000000000000000000000000000000 ), 42);
+
+
