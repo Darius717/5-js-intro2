6cacbf582492ee63ab07de79f3eacea7b3a6baf1
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-22 16:38:45 +0200

    recMaxNum update

--- js/19-recMaxNum.js
+++ js/19-recMaxNum.js
@@ -1,15 +1,56 @@
 /*
 
-RAsti didziausia skaiciu bet kokio sudetigumo array`juje
+RAsti didziausia skaiciu bet kokio sudetingumo array`juje
 - visi array elementai yra: arba skaiciai, arba array
 - visi skaiciai  yra normalus
 */
 
 function recMaxNum(list) {
-    let big = list(0);
+    let big = -Infinity;
+
+    for (let i = 0; i < list.length; i++) {
+        const num = list[i];
+
+        if (Array.isArray(num)) {
+            // kai teviniame sarase randa array
+            const bigChild = recMaxNum(num);
+            if (bigChild > big) {
+                big = bigChild;
+            }
+
+            } else {
+                //kai teviniame sarase randa skaiciu
+                if (num > big) {
+                    big = num;
+                }
+        }
+    }
+    return big;
 }
 
-const data = [1, -5, 78, 8];
+const data = [ 
+    1,
+     -5, 
+     [
+        -4,
+        [
+            7,
+            [
+                200,
+                50,
+                300
+            ],
+            11
+        ] ,
+        80
+     ],
+      78,
+       8,
+       [
+           99
+       ]
+    ];
+
 
 const biggest = recMaxNum(data);
 console.log(biggest);
