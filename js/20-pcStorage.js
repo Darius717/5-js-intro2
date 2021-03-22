3ccadac99f4bdd18686ecfda72fad20bc3e04e72
Darius717 <79445427+Darius717@users.noreply.github.com>
2021-03-22 11:42:39 +0200

    Storage exercise

---
+++ js/20-pcStorage.js
@@ -0,0 +1,33 @@
+/*
+
+Duota kietojo disko duomenu struktura folder file ir reikia suskaiciuoti kiek vietos uzima visi failai
+
+Folderis uzima nuli vietos.
+*/ 
+
+function storage() {
+    return 0;
+}
+
+const pc = {
+    name = C,
+    type = 'folder',
+    content: [
+    {
+        name: 'Programs files',
+        type: 'folder',
+        content: []
+    },
+    {
+        name: 'Windows',
+        type: 'folder',
+        content: []
+    },
+    {
+        name: 'msdia80.dl',
+        type: 'file',
+        size: 
+        content: []
+        },
+    ]
+}
